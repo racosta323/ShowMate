019031540e2f89824e7f6e62d03e0c78dadec30a
Rene <racosta323@gmail.com>
2024-04-03 09:16:53 -0500

    basic nav functions complete

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,14 +1,20 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Overlay from 'react-bootstrap/Overlay';
+import Popover from 'react-bootstrap/Popover';
 
 function Search(){
 
     const [events, setEvents] = useState([])
     const [value, setValue] = useState('')
+    const [searching, setSearching] = useState(false);
+    const [target, setTarget] = useState(null);
+    const searchRef = useRef(null)
+
 
     let url = 'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=200&size=200&apikey=fOUwYet5E4mUic5tG4q5w3lY1ZMjyi8k'
 
@@ -24,6 +30,9 @@ function Search(){
 
 
     let eventsSearch = () => {
+        if (!searching) {
+            return null;
+          }
         return events.filter(event => {
             // console.log(event.name)
             const searchTerm = value.toLowerCase()
@@ -32,7 +41,7 @@ function Search(){
         }).map((event)=>{
             console.log(event.name)
             return(
-                <div className='list-group'>
+                <div className='list-group' key={event.id}>
                     <a href="#" className='list-group-item list-group-item-action' aria-current="true">
                         <div className='d-flex w-100 justify-content-between'>
                             <h5 class="mb-1">{event.name}</h5>
@@ -45,26 +54,11 @@ function Search(){
         })
     }
 
-    // let eventsSearch = () => {
-    //     return events.map((event)=>{
-    //         console.log(event.name)
-    //         return(
-    //             <div className='list-group'>
-    //                 <a href="#" className='list-group-item list-group-item-action' aria-current="true">
-    //                     <div className='d-flex w-100 justify-content-between'>
-    //                         <h5 class="mb-1">{event.name}</h5>
-    //                         <p class="mb-1">Some placeholder content in a paragraph.</p>
-    //                         <small>3 days ago</small>
-    //                     </div>
-    //                 </a>
-    //             </div>
-    //         )
-    //     })
-    // }
-
 
     const searchChange =(event) => {
         setValue(event.target.value)
+        setSearching(true);
+        setTarget(searchRef.current);
     }
 
     const submitSearch = (searchTerm) => {
@@ -73,7 +67,7 @@ function Search(){
     }
 
     return(
-        <div>
+        <div className='search-container'>
             <Form inline>
                 <Row>
                     <Col xs={8}>
@@ -84,8 +78,13 @@ function Search(){
                             data-bs-theme="light"
                             value={value}
                             onChange={searchChange}
+                            ref={searchRef}
                         />
-                        {eventsSearch()}
+                        {searching && (
+                        <div className='search-results' style={{ display: searching ? 'block' : 'none', top: `${searchRef.current?.offsetTop + searchRef.current?.offsetHeight}px`, width: `${searchRef.current?.offsetWidth}px` }}>
+                            {eventsSearch().length > 0 ? eventsSearch() : <p>No results found</p>}
+                        </div>
+                        )}
                     </Col>
                     <Col>
                         <Button variant="success" type="submit" onClick={()=>submitSearch(value)}>Submit</Button>
