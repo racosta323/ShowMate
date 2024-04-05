3a305a980134b77aa5f9ad9a4546a56d373c1983
Rene <racosta323@gmail.com>
2024-04-05 11:15:01 -0500

    introduces formik

--- client/src/components/CreateProfile.js
+++ client/src/components/CreateProfile.js
@@ -5,6 +5,7 @@ import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { Formik } from 'formik'
 
 function CreateProfile(){
 
@@ -12,6 +13,31 @@ function CreateProfile(){
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+   const formik = useFormik({
+        initialValues: {
+            name: '',
+            genre: ''
+        },
+        onSubmit: (values) => {
+            fetch('/artists',{
+                method: 'POST',
+                headers: {
+                    "Content-Type": 'application/json'
+                },
+                body: JSON.stringify(values)
+            }).then (resp=> {
+                if(resp.ok){
+                    resp.json().then(artist=>{
+                        console.log(artist)
+                    })
+                } else {
+                    console.log('errors? handle them')
+                }
+            })
+        }
+
+   })
 
     return(
         <>
@@ -27,12 +53,14 @@ function CreateProfile(){
                 <Modal.Title>Create an Artist Profile</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                <Form>
+                <Form onSubmit={formik.handleSubmit}>
                     <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                         <Form.Label>Artist Name</Form.Label>
                         <Form.Control
                             type="input"
                             placeholder="name"
+                            onChange={formik.handleChange}
+                            value={formik.values.name}
                             autoFocus
                         />
                     </Form.Group>
@@ -44,6 +72,8 @@ function CreateProfile(){
                         <Form.Control
                             type='input'
                             placeholder='genre'
+                            onChange={formik.handleChange}
+                            value={formik.values.genre}
                         />
                     </Form.Group>
                     
