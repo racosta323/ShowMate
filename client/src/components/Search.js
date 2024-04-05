e3c594232dbbef675292e15c86b57c822c8edacb
Rene <racosta323@gmail.com>
2024-04-04 20:43:06 -0500

    adds create a profile section

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -11,15 +11,18 @@ function Search(){
         <Container className='py-5'>
                 <Row className='py-5'>
                     <Col xs={8}>
-                        <h2>Search 'Search Term'</h2>
-                        <hr></hr>
+                        <h2 className='fs-1'>Search 'Search Term'</h2>
+                        <hr className='mb-5'></hr>
+                        
                         <SearchList/>
                         <SearchList/>
                         <SearchList/>
                         <SearchList/>
+                        <p className='text-end'>Don't see what you're looking for? </p>
+                        <p className='text-end fs-4 mb-5'>Create a profile (ICON)</p>
                     </Col>
                     <Col className='py-5'>   
-                        <Row className='bg-body-secondary p-5'>
+                        <Row className='bg-body-secondary p-5 mt-4'>
                             <h2>Something could go here</h2>
                         </Row>
                     </Col>
