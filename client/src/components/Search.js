6461664c8995f3b8fa9c85279d2140bda4e1b3bd
Rene <racosta323@gmail.com>
2024-04-04 20:33:04 -0500

    rearranges search page

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -2,22 +2,29 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 
+
 import SearchList from './SearchList'
 
 function Search(){
 
     return(
-        <Container>
-            <Row></Row>
-            <Row className='m-5'></Row>
-            <Row className='my-5'>
-                <Col>
-                    <h2>Search 'Search Term'</h2>
-                </Col>
-            </Row>
-            <Row>
-                <SearchList/>
-            </Row>
+        <Container className='py-5'>
+                <Row className='py-5'>
+                    <Col xs={8}>
+                        <h2>Search 'Search Term'</h2>
+                        <hr></hr>
+                        <SearchList/>
+                        <SearchList/>
+                        <SearchList/>
+                        <SearchList/>
+                    </Col>
+                    <Col className='py-5'>   
+                        <Row className='bg-body-secondary p-5'>
+                            <h2>Something could go here</h2>
+                        </Row>
+                    </Col>
+                </Row>
+               
         </Container>
     )
 }
--- client/src/components/SearchList.js
+++ client/src/components/SearchList.js
@@ -1,23 +1,23 @@
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
+import Image from 'react-bootstrap/Image'
 
 function SearchList(){
     return(
         <Container>
-            <Row className='my-2 bg-body-secondary border-start border-5 border-danger p-3'>
+            <Row className='my-2 bg-body-secondary border-start border-5 border-danger p-2'>
                 <Col>
-                    <img
+                    <Image
                         src="https://s1.ticketm.net/dam/a/8cc/0850a9c7-f269-4506-87f5-0acb3e2e08cc_SOURCE"
-                        height={150}
-                        width={200}
+                        height={100}
+                        width={100}
+                        roundedCircle
                     />
                 </Col>
-                <Col>
-                    <h3>Name</h3>
-                </Col>
-                <Col>
-                
+                <Col xs={10}>
+                    <h2 className='fs-4 fw-bold mt-3'>Name</h2>
+                    <p className='fs-7'>Genre</p>
                 </Col>
             </Row>
         </Container>
