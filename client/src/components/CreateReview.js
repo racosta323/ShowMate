c81ca66182ac5e5b5f31d196a4f75124a298526d
Rene <racosta323@gmail.com>
2024-04-06 12:50:12 -0500

    merges with search branch and copies create profile modal

--- client/src/components/CreateReview.js
+++ client/src/components/CreateReview.js
@@ -1,49 +1,98 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import Form from 'react-bootstrap/Form'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { useFormik } from 'formik'
 
-function CreateProfile(){
+
+function CreateReview(){
+
+    const navigate = useNavigate()
 
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+   const formik = useFormik({
+        initialValues: {
+            id: '',
+            name: '',
+            genre: ''
+        },
+        onSubmit: async (values) => {
+            try{
+                const artistResponse = await fetch('/artists',{
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": 'application/json'
+                    },
+                    body: JSON.stringify(values, null, 2)
+                })
+                if(artistResponse.status ===201){
+                    const artistData = await artistResponse.json()
+                    formik.values.id = artistData.id
+                }
+                // .then (resp=> {
+                //     if(resp.ok){
+                //         resp.json().then(artist=>{
+                //             console.log(artist)
+                //         })
+                //     } else {
+                //         console.log('errors? handle them')
+                //     }
+                // })
+            } catch(error){
+
+            }
+            navigate(`/profile/${formik.values.id}`)
+            window.location.reload()
+        }
+   })
+
     return(
         <>
             <Row >
                 <Col className="d-flex justify-content-end">
                     <Button variant="dark" onClick={handleShow}>
-                        Write a review
+                        Create a Profile
                     </Button>
                 </Col>
             </Row>
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Review a show</Modal.Title>
+                    <Modal.Title>Create an Artist Profile</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                 <Form>
                     <Form.Group className="mb-3" controlId="name">
                         <Form.Label>Artist Name</Form.Label>
                         <Form.Control
-                            type="input"
+                            as="input"
+                            type="name"
+                            name='name'
                             placeholder="name"
+                            onChange={formik.handleChange}
+                            value={formik.values.name}
                             autoFocus
                         />
                     </Form.Group>
                     <Form.Group
                     className="mb-3"
-                    controlId="exampleForm.ControlInput2"
+                    controlId="genre"
                     >
                         <Form.Label>Genre</Form.Label>
                         <Form.Control
-                            type='input'
+                            as="input"
+                            type='genre'
                             placeholder='genre'
+                            name='genre'
+                            onChange={formik.handleChange}
+                            value={formik.values.genre}
                         />
                     </Form.Group>
                     
@@ -53,7 +102,7 @@ function CreateProfile(){
                 <Button variant="danger" onClick={handleClose}>
                     Close
                 </Button>
-                <Button variant="dark" onClick={handleClose}>
+                <Button variant="dark" onClick={formik.handleSubmit}>
                     Save Changes
                 </Button>
                 </Modal.Footer>
@@ -62,4 +111,4 @@ function CreateProfile(){
     )
 }
 
-export default CreateProfile
+export default CreateReview
