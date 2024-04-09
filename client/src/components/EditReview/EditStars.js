7fcb83ac4beba772ebc513808804f03f694d9427
Rene <racosta323@gmail.com>
2024-04-09 15:22:39 -0500

    re-styles stars

--- client/src/components/EditReview/EditStars.js
+++ client/src/components/EditReview/EditStars.js
@@ -2,12 +2,13 @@ import { useState, useRef } from 'react'
 
 import Container from 'react-bootstrap/Container'
 import Stack from 'react-bootstrap/Stack'
+import Col from 'react-bootstrap/Col'
 
 
 function EditStars({ text, formik, reviews }){
 
     const [isEditMode, setEditMode] = useState(false)   
-    const [inputClass, setInputClass] = useState('border border-0 bg-light')
+    const [inputClass, setInputClass] = useState('border border-0 bg-light fs-4 fw-bold')
     
 
     const inputRef = useRef(null)
@@ -26,8 +27,37 @@ function EditStars({ text, formik, reviews }){
     return(
         <Container>
             <form>
-                <Stack direction='horizontal'>
-                    <h6 
+                
+                <div>
+                    <Stack direction='horizontal'>
+                        <Col className='d-flex justify-content-end ms-5'>
+                            <input
+                                ref={inputRef}
+                                type={text}
+                                value={formik.values[text]}
+                                readOnly={!isEditMode}
+                                className={inputClass}
+                                onBlur={()=>{
+                                    setEditMode(false)
+                                    setInputClass('border border-0 bg-light fs-4 fw-bold')
+                                }}
+                                style={{ width: '40px' }} 
+                                onChange={formik.handleChange}
+                            />
+                        </Col>
+                       
+                        <p className='mt-3'>
+                            <span className='text-warning'>
+                            / 5
+                            </span>
+                        </p>
+                        <i className="bi bi-star-fill text-warning ms-2" ></i>
+                        <i as="button" className="ms-3 bi bi-pencil-fill pencil" onClick={turnOnEdit}></i>
+                    </Stack>
+                </div>
+                
+                   
+                    {/* <h6 
                         // className="text-end mt-3"
                         as="input"
                         ref={inputRef}
@@ -51,8 +81,8 @@ function EditStars({ text, formik, reviews }){
                             5 {' '}
                         </span>
                         <i className="bi bi-star-fill text-warning" ></i>
-                        {/* {' '} see review */}
-                    </h6>
+                        {' '} see review
+                    </h6> */}
                     {/* <input
                         ref={inputRef}
                         type={text}
@@ -67,9 +97,9 @@ function EditStars({ text, formik, reviews }){
                         onChange={formik.handleChange}
                         style={{ width: '40px' }} 
                     />                */}
-                    <i as="button" className="ms-2 bi bi-pencil-fill pencil" onClick={turnOnEdit}></i>
                     {/* <i as="button" className="ms-2 bi bi-pencil-fill pencil" onClick={turnOnEdit}></i> */}
-                </Stack>
+                    {/* <i as="button" className="ms-2 bi bi-pencil-fill pencil" onClick={turnOnEdit}></i> */}
+      
             </form>         
         </Container>
     )
