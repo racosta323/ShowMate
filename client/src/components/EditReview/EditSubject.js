58fde569faeeb1926287a259ab970c6502cf4628
Rene <racosta323@gmail.com>
2024-04-16 14:13:58 -0500

    updates styling on subj and stars fields

--- client/src/components/EditReview/EditSubject.js
+++ client/src/components/EditReview/EditSubject.js
@@ -4,6 +4,8 @@ import { useNavigate, useOutletContext, useParams } from 'react-router-dom'
 import Stack from 'react-bootstrap/Stack'
 import Form from 'react-bootstrap/Form'
 import InputGroup from 'react-bootstrap/InputGroup'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function EditSubject({ reviewId, formik }){
 
@@ -58,18 +60,27 @@ function EditSubject({ reviewId, formik }){
                         ></i>
                     </Stack>
                 </> : 
-                <>
-                    <InputGroup>
-                        <Form.Control
-                            ref={inputRef}
-                            type="subject   "
-                            name='subject'
-                            onChange={formik.handleChange}
-                            value={formik.values.subject}
-                            readOnly={!isEditMode}
-                            onBlur={()=>{setEditMode(false)}}
-                        />
-                    </InputGroup>
+                <Row>
+                    <Col xs={10}>
+                        <InputGroup>
+                            <Form.Control
+                                ref={inputRef}
+                                type="subject"
+                                name='subject'
+                                onChange={formik.handleChange}
+                                value={formik.values.subject}
+                                readOnly={!isEditMode}
+                                onBlur={()=>{setEditMode(false)}}
+                            />
+                        </InputGroup>
+                    </Col>
+                    <Col>
+                        <i 
+                            as="button" 
+                            onClick={turnOnEdit}
+                            className="bi bi-pencil-fill pencil"
+                        ></i>
+                    </Col>
                     {/* <input
                         ref={inputRef}
                         type="subject"
@@ -83,12 +94,8 @@ function EditSubject({ reviewId, formik }){
                             setInputClass('border border-0 bg-light')
                         }}
                     /> */}
-                    <i 
-                        as="button" 
-                        onClick={turnOnEdit}
-                        className="ms-2 bi bi-pencil-fill pencil"
-                    ></i>
-                </>
+                    
+                </Row>
             } 
             </form>         
         </div>
