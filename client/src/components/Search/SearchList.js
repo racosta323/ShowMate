751aa85002d9a2efbd23bfbb8025b46443b4dc24
Rene <racosta323@gmail.com>
2024-04-16 17:23:25 -0500

    minor changes to search buttons

--- client/src/components/Search/SearchList.js
+++ client/src/components/Search/SearchList.js
@@ -11,28 +11,26 @@ function SearchList({ name, genre, img, id }){
 
 
     return(
-        <Container>
-            <Row 
-                className='my-2 bg-body-secondary border-start border-5 border-danger p-2'
-                as="button"
-                onClick={()=>{
-                    navigate(`/artists/${id}`)
-                }}
-            >
-                <Col>
-                    <Image
-                        src={img}
-                        height={100}
-                        width={100}
-                        roundedCircle
-                    />
-                </Col>
-                <Col xs={10}>
-                    <h2 className='fs-4 fw-bold mt-3'>{name}</h2>
-                    <p className='fs-7'>{genre}</p>
-                </Col>
-            </Row>
-        </Container>
+        <Row 
+            className='bg-body-secondary border-start border-5 border-danger p-3'
+            as="button"
+            onClick={()=>{
+                navigate(`/artists/${id}`)
+            }}
+        >
+            <Col>
+                <Image
+                    src={img}
+                    height={100}
+                    width={100}
+                    roundedCircle
+                />
+            </Col>
+            <Col xs={11}>
+                <h2 className='fs-4 fw-bold mt-2 text-center ps-3'>{name}</h2>
+                <p className='fs-7 text-center ps-3'>{genre}</p>
+            </Col>
+        </Row>
     )
 }
 
