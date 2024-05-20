e181343aa4b514f60b134fa8aa989525e1619d62
Rene <racosta323@gmail.com>
2024-05-20 16:30:56 -0500

    creates .env file

--- server/config.py
+++ server/config.py
@@ -1,6 +1,9 @@
 import os
 
-from flask import Flask
+from dotenv import load_dotenv
+load_dotenv()
+
+from flask import Flask, render_template
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
@@ -8,10 +11,22 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_bcrypt import Bcrypt
 
-app = Flask(__name__)
+import ipdb
+
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='../client/build',
+    template_folder='../client/build'
+)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("index.html")
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
